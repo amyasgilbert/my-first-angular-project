d8bf3e15030c3b1639a2abebaedaedb1bf5efe80
amyas <amyas.gilbert@room58.com>
2023-11-03 14:21:03 +0000

    made a navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {WonkyComponent} from "./wonky/wonky.component";
+import { MultiWonkComponent } from './multi-wonk/multi-wonk.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        WonkyComponent
+        WonkyComponent,
+        MultiWonkComponent,
+        NavbarComponent
     ],
     imports: [
         BrowserModule
