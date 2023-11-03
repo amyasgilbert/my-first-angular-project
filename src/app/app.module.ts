5185b8ac0097a5ec0d692fe328395ca9e202935b
amyas <amyas.gilbert@room58.com>
2023-11-03 15:02:17 +0000

    cleared the decks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,11 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {WonkyComponent} from "./wonky/wonky.component";
-import { MultiWonkComponent } from './multi-wonk/multi-wonk.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        WonkyComponent,
-        MultiWonkComponent,
-        NavbarComponent
+
     ],
     imports: [
         BrowserModule
--- src/app/wonky/wonky.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-wonky',
-    templateUrl: './wonky.component.html'
-})
-export class WonkyComponent {
-}
